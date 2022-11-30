12f95d60ba3e50dd828b2463919cfa399c1cff20
micriteo <teodorfolea@yahoo.com>
2022-11-30 07:18:52 -0800

    routersploit working

--- main.py
+++ main.py
@@ -5,20 +5,10 @@ import subprocess
 # p1 = subprocess.Popen("ls", )
 
 #subprocess.Popen("ls", shell=True)
-subprocess.call("ls", shell=True, cwd='routersploit', stdout=subprocess.DEVNULL)
-p1 = subprocess.Popen("ls", shell=True, cwd='routersploit', stdout=subprocess.DEVNULL)
+subprocess.call("ls", shell=True, cwd='src/modules/routersploit')
+p1 = subprocess.Popen("ls", shell=True, cwd='src/modules/routersploit', stdout=subprocess.DEVNULL)
 # autopwn = subprocess.run(["run", "/routersploit"], stdout=subprocess.PIPE, cwd='routersploit')
-autopwn = subprocess.run(["python", "rsf.py"], input=b"search autopwn",  cwd='routersploit')
-scan_autopwn = subprocess.Popen(["python", "rsf.py"],
-                                stdin=subprocess.PIPE,
-                                cwd='routersploit')
-scan_autopwn.stdin.write(b"search autopwn\n")
-scan_autopwn.stdin.write(b"show option")
-#subprocess.CompletedProcess(args=["python", "rsf.py"], returncode=0, stdout=b"show options", stderr=b"")
-
-# options2 = subprocess.run(["python", "rsf.py"], input=b"show option",  cwd='routersploit')
-
-
+routersploit = subprocess.run(["python", "rsf.py"], cwd='src/modules/routersploit')
 # p1.wait()
 #
 # if p1.returncode == 0 :
--- src/main.py
+++
@@ -1,9 +0,0 @@
-import views
-
-
-def main():
-    views.main()
-
-
-if __name__ == '__main__':
-    main()
